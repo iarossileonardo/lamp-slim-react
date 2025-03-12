af7960fa8523d1f3988d10b1df608ef8c41d4601
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:39:40 +0100

    primo commit

---
+++ php/controllers/AlunniController.php
@@ -0,0 +1,58 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class AlunniController
+{
+  public function index(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function alunniWithId(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id=$args[id]");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $data = json_decode($request->getBody()->getContents(), true);
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $raw_query = "INSERT INTO alunni (nome, cognome) VALUES ('$data[nome]', '$data[cognome]');";
+    $result = $mysqli_connection->query($raw_query);
+    if($result && $mysqli_connection->affected_rows > 0){
+      $response->getBody()->write(json_encode(array("message"=>"success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+    }
+    return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+  }
+
+  public function update(Request $request, Response $response, $args){
+    $data = json_decode($request->getBody()->getContents(), true);
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $raw_query = "UPDATE alunni";
+    if (isset($data["nome"])) {
+      $raw_query .= " SET nome='$data[nome]'";
+    } elseif (isset($data["cognome"])) {
+      $raw_query .= " SET cognome='$data[cognome]'";
+    } elseif ((isset($data["nome"])) && (isset(data["cognome"]))) {
+      $raw_query .= " SET nome='$data[nome]', cognome='$data[cognome]'";
+    }
+    $raw_query .= " WHERE id='$args[id]';";
+    $result = $mysqli_connection->query($raw_query);
+    if($result && $mysqli_connection->affected_rows > 0){
+      $response->getBody()->write(json_encode(array("message"=>"success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+    }
+    return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+  }
+}
---
+++ php/index.php
@@ -0,0 +1,25 @@
+<?php
+use Slim\Factory\AppFactory;
+
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/HelloController.php';
+require __DIR__ . '/controllers/AlunniController.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello', "HelloController:hello"); //chiama la funzione del controller
+
+$app->get('/hello/{name}', 'HelloController:hello_with_name');
+
+$app->get('/json/{name}', 'HelloController:json_name');
+
+$app->get('/alunni', 'AlunniController:index');
+
+$app->get('/alunni/{id}', 'AlunniController:alunniWithId');
+
+$app->post('/alunni', 'AlunniController:create');
+
+$app->put('/alunni/{id}', 'AlunniController:update');
+
+$app->run();
