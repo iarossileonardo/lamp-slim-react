6c57ecb6966b3c4ff0f3cf7b2565767f73f71c63
lai2-24 <lai2-24@itismeucci.com>
2025-03-26 08:56:19 +0100

    finto delete

--- php/index.php
+++ php/index.php
@@ -22,6 +22,8 @@ $app->post('/alunni', 'AlunniController:create');
 
 $app->put('/alunni/{id}', 'AlunniController:update');
 
-$app->delete('alunni/{id}', 'AlunniController:destroy')
+$app->delete('/alunni/{id}', 'AlunniController:destroy');
 
 $app->run();
+
+?>
