7e15fc2fde0cb35b3eca3d84071f27d3b828ac57
Iarossi Leonardo <iarossi.leonardo@itismeucci.com>
2025-04-01 16:54:56 +0200

    tutto testato

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -4,12 +4,13 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AlunniController
 {
-  public function index(Request $request, Response $response, $args){
+  public function index(Request $request, Response $response, $args)
+  {
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $query = "SELECT * FROM alunni";
     $queryParams = $request->getQueryParams();
     if (isset($queryParams['search'])) {
-      $query .= " WHERE nome LIKE '%" . $queryParams['search'] . "%' OR cognome LIKE '%" . $queryParams['search'] . "%' OR cf LIKE '" . $queryParams['search'] . "'";
+      $query .= " WHERE nome LIKE '%" . $queryParams['search'] . "%' OR cognome LIKE '%" . $queryParams['search'] . "%' OR cf LIKE '%" . $queryParams['search'] . "%'";
     }
 
     if (isset($queryParams['sortCol']) && isset($queryParams['sort'])) {
@@ -23,7 +24,8 @@ class AlunniController
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 
-  public function alunniWithId(Request $request, Response $response, $args){
+  public function alunniWithId(Request $request, Response $response, $args)
+  {
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id=$args[id]");
     $results = $result->fetch_all(MYSQLI_ASSOC);
@@ -32,62 +34,66 @@ class AlunniController
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 
-  public function create(Request $request, Response $response, $args){
+  public function create(Request $request, Response $response, $args)
+  {
     $data = json_decode($request->getBody()->getContents(), true);
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $raw_query = "INSERT INTO alunni (nome, cognome, cf) VALUES ('$data[nome]', '$data[cognome]', '$data[cf]');";
     $result = $mysqli_connection->query($raw_query);
-    if($result && $mysqli_connection->affected_rows > 0){
-      $response->getBody()->write(json_encode(array("message"=>"success")));
+    if ($result && $mysqli_connection->affected_rows > 0) {
+      $response->getBody()->write(json_encode(array("message" => "success")));
     } else {
-      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+      $response->getBody()->write(json_encode(array("message" => $mysqli_connection->error)));
     }
     return $response->withHeader("Content-Type", "application/json")->withStatus(200);
   }
 
-  public function update(Request $request, Response $response, $args){
+  public function update(Request $request, Response $response, $args)
+  {
     $data = json_decode($request->getBody()->getContents(), true);
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $raw_query = "UPDATE alunni SET";
     $prima_aggiunta = true;
     if (isset($data["nome"])) {
       if (!$prima_aggiunta) {
-        $query .= ", ";
+        $raw_query .= ",";
       }
-      $raw_query .= "nome='$data[nome]'";
+      $raw_query .= " nome='$data[nome]'";
       $prima_aggiunta = false;
-    } elseif (isset($data["cognome"])) {
+    }
+    if (isset($data["cognome"])) {
       if (!$prima_aggiunta) {
-        $query .= ", ";
+        $raw_query .= ",";
       }
-      $raw_query .= "cognome='$data[cognome]'";
+      $raw_query .= " cognome='$data[cognome]'";
       $prima_aggiunta = false;
-    } elseif ((isset($data["cf"]))) {
+    }
+    if ((isset($data["cf"]))) {
       if (!$prima_aggiunta) {
-        $query .= ", ";
+        $raw_query .= ",";
       }
-      $raw_query .= "cf='$data[cf]'";
+      $raw_query .= " cf='$data[cf]'";
       $prima_aggiunta = true;
     }
     $raw_query .= " WHERE id=$args[id]";
     $result = $mysqli_connection->query($raw_query);
-    if($result && $mysqli_connection->affected_rows > 0){
-      $response->getBody()->write(json_encode(array("message"=>"success")));
+    if ($result && $mysqli_connection->affected_rows > 0) {
+      $response->getBody()->write(json_encode(array("message" => "success")));
     } else {
-      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+      $response->getBody()->write(json_encode(array("message" => $mysqli_connection->error)));
     }
     return $response->withHeader("Content-Type", "application/json")->withStatus(200);
   }
 
-  public function destroy(Request $request, Response $response, $args){
-    $data = json_decode($request->getBody()->getContents(), true);
+  public function destroy(Request $request, Response $response, $args)
+  {
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $raw_query = "DELETE FROM alunni WHERE id=$args[id]";
     $result = $mysqli_connection->query($raw_query);
-    if($result && $mysqli_connection->affected_rows > 0){
-      $response->getBody()->write(json_encode(array("message"=>"success")));
+    if ($result && $mysqli_connection->affected_rows > 0) {
+      $response->getBody()->write(json_encode(array("message" => "success")));
     } else {
-      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+      $response->getBody()->write(json_encode(array("message" => $mysqli_connection->error)));
     }
     return $response->withHeader("Content-Type", "application/json")->withStatus(200);
   }
--- php/index.php
+++ php/index.php
@@ -18,10 +18,14 @@ $app->get('/alunni', 'AlunniController:index');
 
 $app->get('/alunni/{id}', 'AlunniController:alunniWithId');
 
+
+//curl -X POST localhost:8080/alunni -H "Content-Type: application/json" -d "{ \"nome\":\"leo\", \"cognome\":\"jarro\", \"cf\":\"26ia\" }"
 $app->post('/alunni', 'AlunniController:create');
 
+//curl -X PUT localhost:8080/alunni/6 -H "Content-Type: application/json" -d "{ \"nome\":\"leolino\" }"
 $app->put('/alunni/{id}', 'AlunniController:update');
 
+//curl -X DELETE localhost:8080/alunni/2
 $app->delete('/alunni/{id}', 'AlunniController:destroy');
 
 $app->run();
